f203aaa246bed4a0986a852ed0fddd0380307bda
hibasalem <salemhiba.hs@gmail.com>
2021-07-14 00:36:52 +0300

    finished till id

--- src/components/Join.js
+++ src/components/Join.js
@@ -14,13 +14,16 @@ class Join extends Component {
       showGame: false,
       gamesArr: [],
       onlineGamers: [],
+      // gameId: '',
     };
   }
   componentDidMount() {
     socket.on('connect', () => {
       console.log('Hello from connect join');
       socket.emit('join', { name: this.props.playerName });
+
       socket.emit('getAll');
+
       socket.on('newGame', (payload) => {
         console.log('Before', payload, socket.id);
         this.setState({ gamesArr: [...this.state.gamesArr, payload] });
@@ -33,19 +36,23 @@ class Join extends Component {
         this.setState({
           onlineGamers: this.state.onlineGamers.filter(
             (gamers) => gamers.id !== payload.id
-          )
+          ),
         });
       });
     });
   }
 
-  handleJoin = (id, socketId) => {
-    console.log('hello handle join');
-    socket.emit('claim', {
-      id,
+  handleJoin = (playerName, gameId) => {
+    let userId = prompt('enter the game id');
+    // this.setState({ gameId: userId });
+    console.log('hello handle join', userId);
+
+    let claimPayload = {
       name: this.props.playerName,
-      playerId: socketId,
-    });
+      gameId: this.state.userId,
+    };
+
+    socket.emit('claim', claimPayload);
   };
 
   // updateData = (event) => {
@@ -59,39 +66,40 @@ class Join extends Component {
 
   render() {
     return (
-      <div>
-        {/* {this.state.hideForm && (
-          <form onSubmit={(event) => this.updateData(event)}>
-            <label for="name">what is your name</label>
-            <input type="text" name="name" />
-            <input type="submit" />
-          </form>
-        )} */}
-        {/* {this.state.showName && <h2>Hello {this.state.player2Name}</h2>} */}
+      <>
+        {this.props.showJoin && (
+          <div>
+            {/* {this.state.hideForm && (
+            <form onSubmit={(event) => this.updateData(event)}>
+              <label for="name">what is your name</label>
+              <input type="text" name="name" />
+              <input type="submit" />
+            </form>
+          )} */}
+            {/* {this.state.showName && <h2>Hello {this.state.player2Name}</h2>} */}
 
-        <h3>Choose Your Game :</h3>
-        <div>
-          {this.state.gamesArr.map((game, idx) => {
-            return (
-              <Games
-                {...game}
-                handleJoin={this.handleJoin}
-                idx={idx}
-                key={game.id}
-              />
-            );
-          })}
-        </div>
-        <aside>
-          <h3>available gamers :</h3>
-          {this.state.onlineGamers.map((gamer) => {
-            return (
-
-              <h2 key={gamer.id}>{gamer.name}</h2>
-            )
-          })}
-        </aside>
-      </div>
+            <h3>Choose Your Game :</h3>
+            <div>
+              {this.state.gamesArr.map((game, idx) => {
+                return (
+                  <Games
+                    {...game}
+                    handleJoin={this.handleJoin}
+                    idx={idx}
+                    key={game.id}
+                  />
+                );
+              })}
+            </div>
+            <aside>
+              <h3>available gamers :</h3>
+              {this.state.onlineGamers.map((gamer) => {
+                return <h2 key={gamer.id}>{gamer.name}</h2>;
+              })}
+            </aside>
+          </div>
+        )}
+      </>
     );
   }
 }
